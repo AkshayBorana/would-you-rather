60427edd90ed2599298bb1f447114c6ed9b29b90
Akshay Borana <akshayborana1@gmail.com>
2020-06-10 16:13:17 -0400

    feat: built handleinitialdata func to get data, and combineReducers to pass all reducer funcs.

--- src/actions/shared.js.js
+++ src/actions/shared.js.js
@@ -1,2 +1,16 @@
 import { getInitialData } from '../utils/API';
+import setAuthUser from './authUser';
+import setUsers from './users';
+import setQuestions from './questions';
 
+// Thunk Action creator...
+export default function handleInitailData() {
+    return (dispatch) => {
+        return getInitialData()
+                .then(({users, questions}) => {
+                    // get users and questions and adding them to the Redux store...
+                    dispatch(setUsers(users));
+                    dispatch(setQuestions(questions));
+                })
+    }
+}
