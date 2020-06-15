86bfbfcb154b0ca140437012e5bdf11a8b16d2a3
Akshay Borana <akshayborana1@gmail.com>
2020-06-14 21:26:06 -0400

    feat: built action creators to add a question.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,29 @@
+import { saveQuestion} from "../utils/API";
+import { addUsersQuestion } from "./users";
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function receiveQuestions(questions) {
    return {
         type: RECEIVE_QUESTIONS,
         questions
+   }
+}
+
+export function addQuestion(question) {
+   return {
+      type: ADD_QUESTION,
+      question
+   }
+}
+
+export function handleSaveQuestion(optionOne, optionTwo, authUser) {
+   return (dispatch) => {
+      return saveQuestion({optionOne, optionTwo, authUser})
+               .then(res => {
+                  dispatch(addQuestion(res));
+                  dispatch(addUsersQuestion(res));
+               })
    }
 }
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,17 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const ADD_USERS_QUESTION = 'ADD_USERS_QUESTION';
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function addUsersQuestion({id, user}) {
+    return {
+        type: ADD_USERS_QUESTION,
+        id,
+        user
     }
 }
